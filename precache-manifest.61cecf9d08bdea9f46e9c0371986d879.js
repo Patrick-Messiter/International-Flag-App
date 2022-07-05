839b892e0ea776d059fbae7de14ac3d77b780bac
Patrick Messiter <Patrick.Messiter@gmail.com>
2022-07-05 17:42:56 +1000

    Updates

---
+++ precache-manifest.61cecf9d08bdea9f46e9c0371986d879.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "11f497f94141d1779e3c",
+    "url": "/International-Flag-App/static/css/main.e30df072.chunk.css"
+  },
+  {
+    "revision": "11f497f94141d1779e3c",
+    "url": "/International-Flag-App/static/js/main.11f497f9.chunk.js"
+  },
+  {
+    "revision": "b8cd82334b200a307fc7",
+    "url": "/International-Flag-App/static/js/1.b8cd8233.chunk.js"
+  },
+  {
+    "revision": "954fdbe44c8e44a80ec4",
+    "url": "/International-Flag-App/static/js/runtime~main.954fdbe4.js"
+  },
+  {
+    "revision": "1167a5e782ac798e894c91d47f448404",
+    "url": "/International-Flag-App/index.html"
+  }
+];
